4a646c75ddef5bedcc6002c177e05d1772455596
Andy Librian <andylibrian@gmail.com>
2014-04-19 12:15:31 +0700

    Docblock : Dictionary Interface

--- src/Sastrawi/Dictionary/DictionaryInterface.php
+++ src/Sastrawi/Dictionary/DictionaryInterface.php
@@ -1,14 +1,27 @@
 <?php
+/**
+ * Sastrawi (https://github.com/sastrawi/sastrawi)
+ *
+ * @link      http://github.com/sastrawi/sastrawi for the canonical source repository
+ * @license   https://github.com/sastrawi/sastrawi/blob/master/LICENSE The MIT License (MIT)
+ */
 
 namespace Sastrawi\Dictionary;
 
+/**
+ * The Dictionary interface used by the stemmer.
+ *
+ * @since  0.1.0
+ * @author Andy Librian <andylibrian@gmail.com>
+ */
 interface DictionaryInterface extends \Countable
 {
     /**
-     * Lookup a word, return the word if found
+     * Search for a word in the dictionary.
      *
-     * @param  string      $word the word to look up for
-     * @return string|null
+     * @param string $word The word to search for.
+     *
+     * @return string|null String If the dictionary contains the word, null otherwise.
      */
     public function lookup($word);
 }
