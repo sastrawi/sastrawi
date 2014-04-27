873ebbdda425ca8dc15c1c5c3f4d15a5d9beda3f
Andy Librian <andylibrian@gmail.com>
2014-04-27 16:04:01 +0700

    insert file header

--- src/Sastrawi/Morphology/InvalidAffixPairSpecification.php
+++ src/Sastrawi/Morphology/InvalidAffixPairSpecification.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Sastrawi (https://github.com/sastrawi/sastrawi)
+ *
+ * @link      http://github.com/sastrawi/sastrawi for the canonical source repository
+ * @license   https://github.com/sastrawi/sastrawi/blob/master/LICENSE The MIT License (MIT)
+ */
 
 namespace Sastrawi\Morphology;
 
